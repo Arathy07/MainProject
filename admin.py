29a74c9df11e66141689b713f3eb4c6d79c8df06
Arathy07 <aarathyps0984@gmail.com>
2024-02-19 12:31:27 +0530

    view,update,delete

--- admin.py
+++ admin.py
@@ -33,8 +33,37 @@ def adminmanagement():
     return render_template("adminmanagement.html",data=data)
 
 
-@admin.route("/diseasemanaging")
+@admin.route("/diseasemanaging",methods=['get','post'])
 def diseasemanagement():
-   
+    data={}
+    qry3="select * from diseases"
+    data['user']=select(qry3)
+    
+    if 'submit' in request.form:
+        Diseasename=request.form['Diseasename']
+        description=request.form['description']
+        uploadimage=request.form['image']
+        qry1="insert into diseases values(null,'%s','%s','%s')"%(Diseasename, description,uploadimage)
+        insert(qry1)
 
-    return render_template("diseasemanagement.html")
+    if 'action'in request.args:
+        action=request.args['action']
+        id=request.args['id']
+
+
+        if action=='delete':
+            qry2="delete from diseases where disease_id='%s'"%(id)
+            delete(qry2)
+        if action=='update':
+             qry6="select * from diseases where disease_id='%s'"%(id)
+             data['up']=select(qry6)
+             if 'update' in request.form:
+                 Diseasename=request.form['Diseasename']
+                 description=request.form['description']
+                 uploadimage=request.form['image']
+                 q="update diseases set disease_name='%s',description='%s',image='%s' where disease_id='%s'"%( Diseasename,description,uploadimage,id)
+                 update(q)
+                    
+        
+
+    return render_template("diseasemanagement.html",data=data)
